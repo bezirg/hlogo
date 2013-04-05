59decefc35acf1d471711acec0b1a94748854035
Nikolaos Bezirgiannis <bezirg@gmail.com>
2013-04-05 14:46:07 +0200

    Added custom benchmarks

---
+++ bench/HLogo/Custom1/Custom1.hs
@@ -0,0 +1,42 @@
+module Main where
+
+import Framework.Logo.Keyword
+import Framework.Logo.Prim
+import Framework.Logo.Exception
+import Framework.Logo.Base
+import Control.Monad
+
+globals []
+patches_own []
+turtles_own []
+
+setup = do
+  atomic $ create_turtles 100000
+  atomic $ reset_ticks
+
+go = forever $ do
+  t <- unsafe_ticks
+  when (t==8) $ stop
+  ask_ (behave) =<< unsafe_turtles
+  unsafe_show_ t
+  atomic $ tick
+
+behave = do
+   atomic $ do 
+           (forward 1 >> forward 1)
+           (back 1 >> forward 1)
+
+   atomic $ do 
+           (forward 1 >> forward 1)
+           (back 1 >> forward 1)
+
+   atomic $ do 
+           (forward 1 >> forward 1)
+           (back 1 >> forward 1)
+
+   atomic $ do 
+           (forward 1 >> forward 1)
+           (back 1 >> forward 1)
+
+
+run ['setup, 'go]
---
+++ bench/HLogo/Custom2/Custom2.hs
@@ -0,0 +1,39 @@
+module Main where
+
+import Framework.Logo.Keyword
+import Framework.Logo.Prim
+import Framework.Logo.Exception
+import Framework.Logo.Base
+import Control.Monad
+
+globals []
+patches_own []
+turtles_own []
+
+setup = do
+  atomic $ create_turtles 100000
+  atomic $ reset_ticks
+
+go = forever $ do
+  ask_ (behave >> behave >> behave >> behave >> behave >> behave >> behave >> behave) =<< unsafe_turtles
+  stop
+
+behave = do
+   atomic $ do 
+           (forward 1 >> forward 1)
+           (back 1 >> forward 1)
+
+   atomic $ do 
+           (forward 1 >> forward 1)
+           (back 1 >> forward 1)
+
+   atomic $ do 
+           (forward 1 >> forward 1)
+           (back 1 >> forward 1)
+
+   atomic $ do 
+           (forward 1 >> forward 1)
+           (back 1 >> forward 1)
+
+
+run ['setup, 'go]
